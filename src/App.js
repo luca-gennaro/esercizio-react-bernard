e0afc1281da0161f11e7cd1082feba50b98281c7
Bernard Amankwah <bernardamankwah@hotmail.com>
2022-08-30 10:13:00 +0200

    props and comp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import {Fragment} from 'react';
+import { DATA } from "./database";
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const items = DATA;
+  return ( 
+    <Fragment>
+       <header>
+      <nav>
+        <div className="logo">LOGO</div>
+        <ul className="link">
+          <li>Home</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+    <div className="slider">REACT PROPS AND COMPONENTS</div>
+    <ul className="item">
+      {items.map(item=>
+      <li>
+        <div className="card">
+        <div className="cover">
+          <img src={item.image} alt="" />
+        </div>
+        <h3 className="">{item.name}</h3>
+        <div className="price">€{item.price}</div>
+        </div>
+      </li>)}
+    </ul>
+    </main>
+    </Fragment>
+   );
 }
-
+ 
 export default App;
---
+++ src/database.js
@@ -0,0 +1,52 @@
+export const DATA = [
+    {
+      id: 1,
+      name: "Bike",
+      price: 100,
+      image:
+        "https://wallpaperheart.com/wp-content/uploads/2018/08/bike-wallpaper-12.jpg",
+    },
+    {
+      id: 2,
+      name: "TV",
+      price: 200,
+      image:
+        "https://th.bing.com/th/id/OIP.UEmMR4OfczDvWIUratzGkQHaFD?pid=ImgDet&rs=1",
+    },
+    {
+      id: 3,
+      name: "Album",
+      price: 10,
+      image:
+        "https://th.bing.com/th/id/R.b1dca103a08fb09ab6093c1843206e62?rik=poC1LUfrAnasRg&riu=http%3a%2f%2fthesaturnherald.com%2fwp-content%2fuploads%2f2019%2f08%2fThe-Beatles.png&ehk=fRCOKcIZLuTKJTDe5UT%2fU0FlIEhc5Cv3L6jFWg5Rwmk%3d&risl=&pid=ImgRaw&r=0",
+    },
+    {
+      id: 4,
+      name: "Book",
+      price: 5,
+      image:
+        "https://th.bing.com/th/id/OIP.o2UpCnJR1l2GyvdN3wBcqwHaJL?pid=ImgDet&rs=1",
+    },
+    {
+      id: 5,
+      name: "Phone",
+      price: 500,
+      image:
+        "https://th.bing.com/th/id/R.ddf77b58ca5a464ab8beb0576111a2a1?rik=pQSCB0LH8fslzA&pid=ImgRaw&r=0",
+    },
+    {
+      id: 6,
+      name: "Computer",
+      price: 1000,
+      image:
+        "https://www.gazabhindi.com/wp-content/uploads/2018/05/%E0%A4%95%E0%A4%82%E0%A4%AA%E0%A5%8D%E0%A4%AF%E0%A5%82%E0%A4%9F%E0%A4%B0.png",
+    },
+    {
+      id: 7,
+      name: "Keyboard",
+      price: 25,
+      image:
+        "https://th.bing.com/th/id/R.a5723d93d4ac34f1a415fabd37b0ea61?rik=VtrekwI2noLTUA&riu=http%3a%2f%2fwww.pngmart.com%2ffiles%2f1%2fDas-Keyboard-Professional-PNG.png&ehk=E7qV2x5dcZ%2f%2baJbMCCQkADCLW1MBNMRqDWwqjJSISDY%3d&risl=&pid=ImgRaw&r=0",
+    },
+  ];
+  
